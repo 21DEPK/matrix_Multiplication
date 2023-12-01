d1cad87a354548d3a5fe8d3ace24c695ccaad063
21DEPK <deepakiaf15@gmail.com>
2023-12-01 11:59:59 +0530

    Latest

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const matrix1Button = document.querySelector("#matrix1");
 const matrix2Button = document.querySelector("#matrix2");
 const matrix1Table = document.querySelector(".matrix1table table");
 const matrix2Table = document.querySelector(".matrix2table table");
+let matrix1 = new Array();
+let matrix2 = new Array();
 
 function getValue() {
   r1 = document.querySelector("#matrix1_rows").value;
@@ -12,14 +14,15 @@ function getValue() {
 }
 
 function createTable1() {
-  getValue();  
+  matrix1Table.style.visibility = "visible";
+  getValue();
   removePreviousTable(matrix1Table);
   document.querySelector("#matrix2_rows").value = c1;
   for (let i = 0; i < r1; i++) {
-    const row = document.createElement("tr");
+    let row = document.createElement("tr");
     for (let j = 0; j < c1; j++) {
-      const column = document.createElement("td");
-      column.setAttribute("id", `t1_cell[${i}${j}]`);
+      let column = document.createElement("td");
+      column.innerHTML = `<input type="number" id="t1${i}${j}" class="cellInput"/> `;
       row.appendChild(column);
     }
     matrix1Table.appendChild(row);
@@ -27,13 +30,14 @@ function createTable1() {
 }
 
 function createTable2() {
+  matrix2Table.style.visibility = "visible";
   getValue();
   removePreviousTable(matrix2Table);
   for (let i = 0; i < r2; i++) {
-    const row = document.createElement("tr");
+    let row = document.createElement("tr");
     for (let j = 0; j < c2; j++) {
-      const column = document.createElement("td");
-      column.setAttribute("id", `t2_cell[${i}${j}]`);
+      let column = document.createElement("td");
+      column.innerHTML = `<input type="number" id="t1${i}${j}" class="cellInput"/> `;
       row.appendChild(column);
     }
     matrix2Table.appendChild(row);
@@ -43,5 +47,15 @@ function createTable2() {
 function removePreviousTable(t) {
   while (t.children.length != 1) {
     t.removeChild(t.childNodes[2]);
+  }
+}
+
+function result() {
+  let result = new Array();
+  for (let i = 0; i < r1; i++) {
+    result[i] = [];
+    for (let j = 0; j < c2; j++) {
+      result[i][j] = j;
+    }
   }
 }
